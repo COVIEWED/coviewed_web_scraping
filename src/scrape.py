742e034e911644c47bc56cf643a675c4ac1e4098
Vanessa Sabino <borboleta@gmail.com>
2020-03-29 21:54:03 -0400

    refactor into smaller methods

--- src/scrape.py
+++ src/scrape.py
@@ -1,48 +1,27 @@
 # -*- coding: utf-8 -*-
-__title__ = 'coviewed_web_scraping with newspaper lib'
-__author__ = 'Dietrich Trautmann'
-__license__ = 'MIT'
-__copyright__ = 'Copyright 2020, Dietrich Trautmann'
+__title__ = "coviewed_web_scraping with newspaper lib"
+__author__ = "Dietrich Trautmann"
+__license__ = "MIT"
+__copyright__ = "Copyright 2020, Dietrich Trautmann"
 
 import os
-import hashlib 
+import hashlib
 import argparse
 from newspaper import Article
 
-if __name__ == '__main__':
 
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-            "-u",
-            "--url",
-            help="please provide a web url to scrape",
-            required=True,
-            type=str
-            )
-    parser.add_argument(
-            "-o",
-            "--output_file",
-            help="provide an alternative name for the ouput_file",
-            type=str
-            )
-    parser.add_argument(
-            "-v",
-            "--verbose",
-            help="shows output",
-            action='store_true'
-            )
-    args = parser.parse_args()
-
-    if args.output_file:
-        output_file = args.output_file
-    else:
-        output_file = hashlib.md5(args.url.encode()).hexdigest() + '.txt'
-        output_file = os.path.join('data', output_file)
-
+def parse_article(url):
     article = Article(args.url)
     article.download()
     article.parse()
-    
+    return article
+
+
+def write_output(article, output_file, verbose=False):
+    if output_file is None:
+        output_file = hashlib.md5(args.url.encode()).hexdigest() + ".txt"
+        output_file = os.path.join("data", output_file)
+
     with open(output_file, "w") as f:
         f.writelines(args.url)
         f.writelines("\n\n")
@@ -52,9 +31,33 @@ if __name__ == '__main__':
         f.writelines("\n\n")
         f.writelines(article.text)
 
-    if args.verbose:
+    if verbose:
         print(output_file)
         print(args.url)
         print(article.title)
         print(article.text)
-    
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-u",
+        "--url",
+        help="please provide a web url to scrape",
+        required=True,
+        type=str,
+    )
+    parser.add_argument(
+        "-o",
+        "--output_file",
+        help="provide an alternative name for the ouput_file",
+        type=str,
+    )
+    parser.add_argument("-v", "--verbose", help="shows output", action="store_true")
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    article = parse_article(args.url)
+    write_output(article, args.output_file, args.verbose)
